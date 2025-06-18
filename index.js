c9f4cc98990e6d10306d85893ece08f070097e22
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-18 17:24:42 +1000

    Run game function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 let lastHole;
+let timeUp;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -17,4 +18,21 @@ function randomHole(holes) {
   }
   lastHole = hole;
   return hole;
+}
+
+function popUp() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) popUp();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  popUp();
+  setTimeout(() => (timeUp = true), 10000);
 }
