e6c384eb7392e60aa15df5f1b871e13fd2bf52fa
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-18 16:36:36 +1000

    Random hole & random time functions

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-console.log("Testing");
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+let lastHole;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+
+  if (lastHole === hole) {
+    console.log("Aah, snap. Same one");
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
