3c4bd3ca73b4f252b53b3d922ed25f50296c4aef
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-27 14:18:39 +1000

    Added progress bar and functionality

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 const levels = document.querySelector("#levels");
+const progress = document.querySelector(".progress");
+const bar = document.querySelector(".progress-bar");
 
 let lastHole;
 let timeUp;
 let score = 0;
 let minimum = 200,
   maximum = 1500;
+let timerInterval;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -42,10 +45,29 @@ function popUp() {
   }, time);
 }
 
+function startTimer() {
+  const start = Date.now();
+  const max = 10000;
+  setInterval(() => {
+    const elapsed = Date.now() - start;
+    const percentage = (elapsed / max) * 100;
+    bar.style.width = percentage + "%";
+    // console.log("s:", seconds, "%", percentage);
+    if (elapsed >= max) {
+      clearInterval(timerInterval);
+      bar.style.width = "0%";
+    }
+  }, 10);
+}
+
 function startGame() {
+  clearInterval(timerInterval);
+  bar.style.backgroundColor = "hsl(211, 52%, 50%)";
+  bar.style.width = "0%";
   scoreBoard.textContent = 0;
   timeUp = false;
   score = 0;
+  startTimer();
   popUp();
   setTimeout(() => (timeUp = true), 10000);
   levels.value = "default"; // resets to default levels on start
